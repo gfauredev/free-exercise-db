9bf2443cacdfa730557c12ad4ef289423147356b
Guilhem Fauré <pro@guilhemfau.re>
2026-03-13 15:45:29 +0100

    build: python tooling

--- flake.nix
+++ flake.nix
@@ -16,29 +16,43 @@
         { pkgs }:
         {
           default = pkgs.mkShell {
-            packages = with pkgs; [
-              # biome
-              # bun # Faster runtime, bundler, package manager
-              cmake
-              cmake-format
-              cmake-language-server
-              cmake-lint
-              check-jsonschema
-              csvkit
-              jq
-              jsonfmt
-              json2yaml
-              taplo # TOML LSP
-              # typescript
-              # typescript-language-server # TS LSP
-              vscode-langservers-extracted # HTML/CSS/JS(ON)
-              yaml-language-server # YAML LSP
-              # deno # More secure runtime
-              # node2nix
-              # nodejs
-              # nodePackages.pnpm
-              # yarn
-            ];
+            packages =
+              with pkgs;
+              [
+                # biome
+                # bun # Faster runtime, bundler, package manager
+                cmake
+                cmake-format
+                cmake-language-server
+                cmake-lint
+                check-jsonschema
+                csvkit
+                jq
+                jsonfmt
+                json2yaml
+                python3
+                ruff # Fast lint and format
+                taplo # TOML LSP
+                # typescript
+                # typescript-language-server # TS LSP
+                ty # Type checker and LSP
+                uv # Fast packages and environment manager
+                vscode-langservers-extracted # HTML/CSS/JS(ON)
+                yaml-language-server # YAML LSP
+                # deno # More secure runtime
+                # node2nix
+                # nodejs
+                # nodePackages.pnpm
+                # yarn
+              ]
+              ++ (with pkgs.python3Packages; [
+                rope # Smart refactoring
+                python-lsp-server # Python LSP
+                pylsp-rope # Rope LSP
+                datetime # Date time
+                numpy # Science & maths tools
+                pandas # Data analysis
+              ]);
           };
         }
       );
